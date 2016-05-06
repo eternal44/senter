cdc4c1c7036b00caef079b17a8a1f5854cd25069
James Youn <jamesyoun710@gmail.com>
2016-05-06 16:21:50 -0700

    (server) Handle server & back services connections
    Or else tests will hang

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 import express from 'express';
 import {join} from 'path';
 import middleware from './config/middleware';
+
+// connect to database
 
 const app = express();
 
@@ -10,6 +16,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(join(__dirname, '../client')));
 
-app.listen(port, () => {
-  console.log('Listening on port ', port)
-})
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log('Listening on port ', port)
+  })
+}
+
+export default app;
