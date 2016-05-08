529d5ae1192f3c557e0a4414044f2f176910b1f0
James Youn <jamesyoun710@gmail.com>
2016-05-08 16:30:40 -0700

    (server) Reinstall dependencies
    Some were in dev or globally installed only

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,11 @@
+import morgan from 'morgan';
 import votableRoutes from '../votable/votableRoutes';
 
 export default (app, express, join) =>{
   const votableRouter = express.Router();
 
   app.use(express.static(join(__dirname, '../client')));
+  app.use(morgan('dev'));
 
   app.use('/api/votables', votableRouter);
 
