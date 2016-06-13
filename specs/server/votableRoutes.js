f2a5b61948ddc360de48b8f085c9956e3582ff7d
James Youn <jamesyoun710@gmail.com>
2016-06-13 15:05:20 -0700

    (test) Only query new votables for a user to votes

--- specs/server/votableRoutes.js
+++ specs/server/votableRoutes.js
@@ -18,11 +18,15 @@ test('Fetch unvoted votables', assert => {
   .expect(200)
   .expect('Content-Type', /json/)
   .end((err, res) => {
+    assert.error(err, 'No error');
+
     const actualVotablesCount = res.body.length;
     const expectedVotablesCount = 20;
-
-    assert.error(err, 'No error');
     assert.same(actualVotablesCount, expectedVotablesCount, 'Retrieve unvoted votables');
+
+    // when #notEqual tests fail their expected value returns 'undefined'
+    const firstItem = JSON.parse(res.text)[0];
+    assert.notEqual(firstItem.name, 'item1', 'First item should not be "item1"')
     assert.end();
   });
 });
