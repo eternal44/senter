cfee73899bc2c5ce9d76ae3f69b46e72e50011a9
James Youn <jamesyoun710@gmail.com>
2016-06-15 13:22:37 -0700

    (client) Add instructions

--- client/components/instructions.js
+++ client/components/instructions.js
@@ -4,7 +4,9 @@ class Instructions extends Component {
   render() {
     return (
       <div>
-        <p>This is how</p>
+      <h3>How to vote</h3>
+        <li>Click on the buttons to up or down vote.</li>
+        <li>You can also use the keyboard arrows as well ('left' for downvote, 'right' for upvote).</li>
       </div>
     )
   }
