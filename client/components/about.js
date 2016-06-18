601154570ed9ba613916c34525f6cb14aaf8d5a9
James Youn <jamesyoun710@gmail.com>
2016-06-18 14:38:21 -0700

    (client) Add styling

--- client/components/about.js
+++ client/components/about.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class About extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
       <h3>Source Code</h3>
         <p>Checkout the <a href="https://www.github.com/eternal44/senter">github page</a> for the source code and project road map.</p>
       </div>
--- client/components/instructions.js
+++ client/components/instructions.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Instructions extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
       <h3>How to vote</h3>
         <li>Click on the buttons to up or down vote.</li>
         <li>You can also use the keyboard arrows as well ('left' for downvote, 'right' for upvote).</li>
