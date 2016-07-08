5b15743e64ff40113738f0fe9efb2ea3cfa6cf20
James Youn <jamesyoun710@gmail.com>
2016-07-08 16:36:29 -0700

    (server) Add POST route for votables

--- server/votable/votableController.js
+++ server/votable/votableController.js
@@ -1,4 +1,11 @@
 import db from './../db/config/init';
+import pgp from 'pg-promise'
+
+function sqlQuery(file) {
+  return new pgp.QueryFile(file)
+}
+
+let sqlInsertNewVotables = sqlQuery('./server/votable/insertVotables.sql', {minify: true})
 
 export default {
   newVotable: function(req, res) {
@@ -22,7 +29,7 @@ export default {
 
   fetchUnvotedVotables: function(req, res) {
     const userID = req.params.userID;
-    db.many(['SELECT name, make, photo_url, votables.id AS votable_id ',
+    db.many(['SELECT pinterest_note, image_url, pinterest_url, votables.id AS votable_id ',
             'FROM votables LEFT JOIN votes ON votables.id = votes.votable_id ',
             'WHERE votables.id NOT IN (SELECT votes.votable_id FROM votes ',
             'WHERE voter=$1) LIMIT 20'].join(''), userID)
@@ -33,5 +40,40 @@ export default {
       next(error)
       throw error
     });
+  },
+
+  postNewVotables: function(req, res) {
+    // more properties on req.body available - just console log
+    // them to see what else is available
+    const userPinterestID = req.body.allPins[0].userPinterestID
+    let queries = []
+
+    return db.tx((t) => {
+      return t.one('SELECT id FROM users WHERE pinterest_id=$1', userPinterestID)
+      .catch((err) => {
+        console.log('User not found')
+        res.status(404).send(err)
+      })
+      .then((data) => {
+        req.body.allPins.forEach((votable) => {
+          const insertParam = {
+            user_id: data.id,
+            pinterest_url: votable.pinterest_URL,
+            image_url: votable.image_URL,
+            pinterest_note: votable.pinterest_note
+          }
+          queries.push(t.none(sqlInsertNewVotables, insertParam))
+        })
+        return t.batch(queries)
+      })
+      .catch(function(err) {
+        console.log('Votables not stored', err)
+        res.status(500).send(err)
+      })
+      .then(() => {
+        res.sendStatus(201)
+      })
+    })
   }
-};
+}
+
--- server/votable/votableRoutes.js
+++ server/votable/votableRoutes.js
@@ -3,5 +3,6 @@ import votableController from './votableController';
 export default app => {
   app.get('/:votableID', votableController.fetchVotable);
   app.get('/users/:userID', votableController.fetchUnvotedVotables);
+  app.post('/', votableController.postNewVotables)
 };
 
